5e620142d2d9a37ec1370f4dcf536a829511f92b
vishwas2310 <32561758+vishwas2310@users.noreply.github.com>
2019-05-31 10:23:28 +0530

    field names changed for data on server

--- scan_data.py
+++ scan_data.py
@@ -10,18 +10,20 @@ def send_scan_results(scan_details):
     specs = platform.uname()
     memory = virtual_memory().total/1024**3
     scan_datetime = scan_details['scan_start_datetime']
-    scan_duration = scan_details['scan_end_datetime']
+    scan_end_datetime = scan_details['scan_end_datetime']
     files_scanned_count = scan_details['total_images_scanned']
     files_explicit_count = scan_details['total_explicit_images']
     scan_type = scan_details['scan_type']
+    complete = "NA"
     data = {
     'specs':str(specs),
     'memory':str(memory),
     'scan_datetime':str(scan_datetime),
-    'scan_duration':str(scan_duration),
+    'scan_end_datetime':str(scan_end_datetime),
     'files_scanned_count':files_scanned_count,
     'files_explicit_count':files_explicit_count,
-    'scan_type':scan_type
+    'scan_type':scan_type,
+    'complete':complete
 }
     data=json.dumps(data, indent=2)
 #     data=json.loads(data)
