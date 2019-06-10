b01b4c3506433a243ff41894eedb14b42b8f32a3
Harshit Rai <harshitrai68@gmail.com>
2019-06-10 14:00:16 +0530

    Final Commit

--- watch.py
+++ watch.py
@@ -8,7 +8,7 @@ from watchdog.events import FileSystemEventHandler
 
 class MyHandler(FileSystemEventHandler):
     def on_modified(self, event):
-    	if(event.src_path.endswith('.jpg') or event.src_path.endswith('.png') or event.src_path.endswith('.mp4') or event.src_path.endswith('.jpg') or event.src_path.endswith('.bmp') ):
+    	if(event.src_path.endswith('.jpg') or event.src_path.endswith('.png') or event.src_path.endswith('.mp4') or event.src_path.endswith('.flv') or event.src_path.endswith('.jpeg') ):
     		print(event.src_path)
         # print(f'event type: {event.event_type}  path : {event.src_path}')
 
@@ -17,7 +17,7 @@ def watch(paths):
     event_handler = MyHandler()
     observer = Observer()
     for i in paths:
-	    observer.schedule(event_handler, path=i, recursive=False)
+	    observer.schedule(event_handler, path=i, recursive=True)
     observer.start()
 
     try:
@@ -27,5 +27,5 @@ def watch(paths):
         observer.stop()
     observer.join()
 
-paths1=['C:\\Users\\Vishals\\Desktop\\','C:\\Users\\Vishals\\Desktop\\UAC\\']
+paths1=['C:\\Users\\g_host\\Downloads\\',"C:\\Users\\g_host\\Desktop\\"]
 watch(paths1)
